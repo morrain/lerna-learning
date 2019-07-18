d6a3f541a00157dae730efb57a86b862bf6f09d5
kongchuiliang <626100676@qq.com>
2019-07-18 15:20:19 +0800

    fix(cli): fix path debug
    affects: @mo-demo/cli

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,5 +1,5 @@
 if (process.env.LOCAL_DEBUG) {
-  require('../src/index') // 如果是调试模式，加载src中的源码
+  require('./src/index') // 如果是调试模式，加载src中的源码
 } else {
-  require('../dist/index') // dist会发到npm
+  require('./dist/index') // dist会发到npm
 }
